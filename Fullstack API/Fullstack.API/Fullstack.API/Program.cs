51307919652a729f1a096cafc68a20becf2bb5a2
panchanan-turuk <pturuk123@gmail.com>
2025-03-03 11:44:21 +0530

    updated to .Net 8

--- Fullstack API/Fullstack.API/Fullstack.API/Program.cs
+++ Fullstack API/Fullstack.API/Fullstack.API/Program.cs
@@ -1,15 +1,20 @@
 using Fullstack.API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+// Configure Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fullstack API", Version = "v1" });
+});
+
+// Configure Entity Framework with SQL Server
 builder.Services.AddDbContext<FullStackDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("FullStackConnectionString")));
 
@@ -19,7 +24,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fullstack API v1"));
 }
 
 app.UseHttpsRedirection();
